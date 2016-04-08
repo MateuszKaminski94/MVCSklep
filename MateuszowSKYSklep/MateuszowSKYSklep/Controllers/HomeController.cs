03bac09e0d9e64a38a3dc2bc48bc8878651db952
Mateusz Kamiński <mtk4991@gmail.com>
2016-04-08 21:51:13 +0200

    8.04 wieczor

--- MateuszowSKYSklep/MateuszowSKYSklep/Controllers/HomeController.cs
+++ MateuszowSKYSklep/MateuszowSKYSklep/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace MateuszowSKYSklep.Controllers
         public ActionResult Index()
         {
             var genres = db.Genres.ToList();
-            var preOrders = db.Games.Where(a => !a.IsHidden && a.IsPreOrder).OrderByDescending(a => a.DateAdded).Take(1).ToList();
+            var preOrders = db.Games.Where(a => !a.IsHidden && a.IsPreOrder).OrderByDescending(a => a.DatePremiere).Take(1).ToList();
             var randoms = db.Games.Where(a => !a.IsHidden).OrderBy(a => Guid.NewGuid()).Take(4).ToList();
 
             var vm = new HomeViewModel()
--- MateuszowSKYSklep/MateuszowSKYSklep/Models/Game.cs
+++ MateuszowSKYSklep/MateuszowSKYSklep/Models/Game.cs
@@ -12,9 +12,15 @@ namespace MateuszowSKYSklep.Models
         public int GenreId { get; set; }
         public string GameTitle { get; set; }
         public string DeveloperName { get; set; }
-        public DateTime DateAdded { get; set; }
+        public DateTime DatePremiere { get; set; }
         public string MainImageFilename { get; set; }
         public string Description { get; set; }
+        public string Source { get; set; }
+        public string Cpu { get; set; }
+        public string Ram { get; set; }
+        public string Gpu { get; set; }
+        public string Hdd { get; set; }
+        public string Os { get; set; }
         public decimal Price { get; set; }
         public bool IsPreOrder { get; set; }
         public bool IsHidden { get; set; }
