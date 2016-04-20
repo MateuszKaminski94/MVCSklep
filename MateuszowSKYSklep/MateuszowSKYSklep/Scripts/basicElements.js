bf729bf6234ff8ca0e91712a5c66ba247d8a4358
Mateusz Kamiński <mtk4991@gmail.com>
2016-04-20 18:12:10 +0200

    20.04 popoludnie

--- MateuszowSKYSklep/MateuszowSKYSklep/Scripts/basicElements.js
+++ MateuszowSKYSklep/MateuszowSKYSklep/Scripts/basicElements.js
@@ -7,7 +7,7 @@
             $('#show').css('background-color', '#32353a');
             triangle.animate({ borderSpacing: 90 }, {
                 step: function (now, fx) {
-                    $('#show img').css('-webkit-transform', 'rotate(' +'-'+ now + 'deg)');
+                    $('#show img').css('-webkit-transform', 'rotate(' + '-' + now + 'deg)');
                     $('#show img').css('-moz-transform', 'rotate(' + '-' + now + 'deg)');
                     $('#show img').css('transform', 'rotate(' + '-' + now + 'deg)');
                 },
@@ -30,7 +30,32 @@
 
     var selectview = function (id) {
 
-        window.location.href = 'game-' + id + '.html';
+        
+        //$(function () {
+        //    $.ajax({
+        //        url: '@(Url.Action("Store", "Details", new {' + id + ' = game.GameId} ))',
+        //        dataType: 'html',
+        //        success: function (data) {
+        //            window.location.assign(html(data));
+        //        },
+        //    });
+        //});
+
+
+        //$.mobile.navigate('@Url.Action("Details", "Store", new {' + id + ' = game.GameId})');
+       
+        var url = document.location.origin + "/game-" + id + ".html";
+        window.location.assign(url);
+       // console.log(url);
+        //window.location.assign("http://localhost:65356/game-" + id + ".html");
+
+        //window.location.assign('@Url.Action("Details", "Store", new {' + id + ' = game.GameId})');
+
+        //window.location.assign('@Url.Action("Details", "Store", new {' + id + ' = game.GameId})');
+        //location.href = 'game-' + id + '.html';
+        //window.location.href = 'Store/Details/' + id;
+        //$("#searchfilter").attr("data-autocomplete-source", '@Url.Action("Details", "Store", new {' + id + ' = game.GameId})');
+        //$("#searchfilter").html('@Url.Action("Details", "Store", new {' + id + ' = game.GameId})');
     };
 
     var setupAutoComplete = function () {
@@ -41,8 +66,11 @@
             select: function (event, ui) {
                 $input = $(this);
                 $input.val(ui.item.label);
-                var $form = $input.parents("form:first");
-                $form.submit(selectview(ui.item.id));
+                //var $form = $input.parents("form:first");
+                //console.log($form);
+                //$form.submit(selectview(ui.item.id));
+                    
+               selectview(ui.item.id);
             }
         };
 
@@ -74,8 +102,8 @@
         }
         //$('#searchfilter').toggle(function() {});
     });
-       
-    
+
+
     //$('#search').toggle(
     //            function () {
     //                $('#search').css('background-color', '#494d55');
